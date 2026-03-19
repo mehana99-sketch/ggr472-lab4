22c53d96f8ebdac2ed0a4f0add22a1889c001d58
mehana_n <mehana99@gmail.com>
2026-03-18 21:51:21 -0400

    major customization and fuctionality
    - added hexgrid to map
    - interpolated hexagon symbology
    - opaque on mouse hover
    - mouse symbology on hover
    - working layer checkboxes
    - filtered out polygons with no collisions
    - resized hexagons
    - navcontrol, fullscreen
    - started css styling

--- README.md
+++ README.md
@@ -1,7 +1,4 @@
 # GGR472 Lab 4: Incorporating GIS analysis into web maps using Turf.js
- 
-This repository contains the starter code required to complete Lab 4. The lab is designed to help you learn how to perform spatial analysis and visualize outputs using the [Turf.js](https://turfjs.org/) and [Mapbox GL JS](https://docs.mapbox.com/mapbox-gl-js/api/) libraries.
-
 
 ## Repository Contents
 - `data/pedcyc_collision_06-21.geojson`: Data file containing point locations of road collisions involving pedestrian and cyclists between 2006 and 2021 in Toronto 
@@ -9,8 +6,27 @@ This repository contains the starter code required to complete Lab 4. The lab is
 - `index.html`: HTML file to render the map
 - `style.css`: CSS file for positioning the map interface
 - `script.js`: JavaScript file template to be updated to include code that creates and visualizes and hexgrid map based on the collision data
-   
 
 ## Tasks
 - Created empty variable, used fetch to fill it.
-- Loaded map, added source and layer.
+- Loaded map, added sources and layers.
+- Created bounding box using 'turf.envelope()'
+    - Resized bounding box using 'turf.transformScale()'
+    - Got usable array (for hexgrid) using 'turf.bbox()'
+- Created hexgrid using 'turf.hexGrid()'
+- Aggregated collision data (count) for each hexagon 
+- Used a loop to iteratively find polygon with most collisions
+- Practiced 'console.log()' to search for values and test code
+    - Note: code is commented out, but I left it there for future reference
+- Further customization: 
+    - Mouse symbol change when hovering over polygons
+    - Fullscreen, navigation control
+    - Hexgrid:
+        - Reduced opacity, increased hexagon size
+        - Interpolated colour intensity for COUNT values between 1 and 122 (122 is maximum count)
+        - in 'addLayer', filtered out polygons where COUNT = 0
+        - Changed opacity to highlight based on hover (needed paint change and event listener)
+    - Checkboxes for each layer's visibility
+
+## Issues
+- How can I easily check the parameters for each tool? I'm still not exactly sure why 'turf.bbox()' works on transformed data, but 'variable.bbox()' doesn't.
--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// 1. INITALIZE MAP
+// INITALIZE MAP
 
 // Define access token
 mapboxgl.accessToken = 'pk.eyJ1IjoibWVoYW5hLW4iLCJhIjoiY21rb2Zxb24wMDVvbzNlcHhhNGwxc3ZpOCJ9.cldXiKJrisAMpXXAL0qobg'; //****ADD YOUR PUBLIC ACCESS TOKEN*****
@@ -12,7 +12,6 @@ const map = new mapboxgl.Map({
 });
 
 // Create variable for GeoJson point data
-
 let injurygeojson; // create EMPTY variable
 fetch('https://raw.githubusercontent.com/mehana99-sketch/ggr472-lab4/refs/heads/main/data/pedcyc_collision_06-21.geojson')
     .then(response => response.json()) // Transforms response into JSON
@@ -21,26 +20,11 @@ fetch('https://raw.githubusercontent.com/mehana99-sketch/ggr472-lab4/refs/heads/
         injurygeojson = response; // store geoJSON as variable from URL (fetched)
     });
 
-// 2. LOAD MAP
+// LOAD MAP
 
 map.on('load', () => {
 
-    // create envelope
-
-    // must have geojson parameter
-    // added 'bbox' to return array of coordinates (in format minX, minY, maxX, maxY)
-    let boundingboxfeature = turf.envelope(injurygeojson);
-    let boundingboxresized = turf.transformScale(boundingboxfeature, 1.1); // resize envelope
-    let boundingbox = turf.bbox(boundingboxresized); // pick out array as parameter for 'turf.hexGrid'
-    // I had consistent issues with 'boundingboxfeature.bbox' (didn't work after 'turf.transformScale'), had to switch to 'turf.bbox(boundingboxfeature)'
-
-    // hexgrid
-
-    // parameters: bbox, cellside (length of side of hexagon), options (optional parameter)
-    let hexgrid = turf.hexGrid(boundingbox, 0.5, { units: "kilometers" });
-
-    // add sources, layers
-
+    // Add point source and layer
     map.addSource('injury-points', {
         type: 'geojson',
         data: injurygeojson // I could put the raw URL here, but I need it to be a separate variable for turf.js
@@ -51,31 +35,33 @@ map.on('load', () => {
         'source': 'injury-points',
         'paint': {
             'circle-radius': 3,
-            'circle-color': 'pink'
+            'circle-color': 'blue'
         }
     });
-    // add hexgrid to map
-    map.addSource('hexgrid-source', {
-        type: 'geojson',
-        data: hexgrid
-    });
-    map.addLayer({
-        'id': 'Tessellation',
-        'type': 'line',
-        'source': 'hexgrid-source'
-        // automatically paints plack
-    });
 
-// 4. AGGREGATE DATA BY HEXGRID
+    // HEXGRID OPERATIONS
 
+    // Create envelope
+    // must have geojson parameter
+    // added 'bbox' to return array of coordinates (in format minX, minY, maxX, maxY)
+    let boundingboxfeature = turf.envelope(injurygeojson);
+    let boundingboxresized = turf.transformScale(boundingboxfeature, 1.1); // resize envelope
+    let boundingbox = turf.bbox(boundingboxresized); // pick out array as parameter for 'turf.hexGrid'
+    // I had consistent issues with 'boundingboxfeature.bbox' (didn't work after 'turf.transformScale'), had to switch to 'turf.bbox(boundingboxfeature)'
+
+    // Create hexgrid
+    // parameters: bbox, cellside (length of side of hexagon), options (optional parameter)
+    let hexgrid = turf.hexGrid(boundingbox, 0.8, { units: "kilometers" });
+
+    // Aggregate data by hexgrid
     // collect points within each polygon
     // parameters: polygon feature collection, point feature collection, 'inProperty' (property to be nested from), 'outProperty' (property to be nested into)
     let agg_collision = turf.collect(hexgrid, injurygeojson, '_id', 'values'); // returns polygons with properties listed
     //console.log(agg_collision);
 
-    // finding polygon with highest count of collisions
+    // Find polygon with highest count of collisions
     let max_collision = 0
-    agg_collision.features.forEach((feature) =>{ // forEach makes iterative
+    agg_collision.features.forEach((feature) => { // forEach makes iterative
         feature.properties.COUNT = feature.properties.values.length // this creates a new field 'COUNT' in properties, and assigns a value (length of values list) for each polygon 
         if (feature.properties.COUNT > max_collision) { // iteratively tests higher value of collision count
             //console.log(feature);
@@ -83,9 +69,91 @@ map.on('load', () => {
         }
     });
     //console.log(max_collision);
-    //returns 55 
+    //returns 122 (was 55 until I made the hexagons bigger) 
 
-    //------------------------------------------------------------------------------
+    // Add and style hexgrid
+    map.addSource('agg_collision', {
+        type: 'geojson',
+        data: hexgrid,
+        'generateId': true // Create a unique ID for each feature
+    });
+    map.addLayer({
+        'id': 'Tessellation',
+        'type': 'fill',
+        'source': 'agg_collision',
+        'paint': {
+            'fill-color': [
+                'interpolate', // adjust polygon rgb colour based on count
+                ['linear'], ["get", 'COUNT'],
+                1, 'rgb(255, 255, 255)',
+                20, 'rgb(255, 182, 248)',
+                50, 'rgb(255, 88, 238)',
+                122, 'rgb(137, 0, 123)'],
+            'fill-opacity': [
+                'case', ['boolean', ['feature-state', 'hover'], false], 1, 0.5]
+            , // CASE and FEATURE STATE expression sets opactity as 0.5 when hover state is false and 1 when updated to true
+            'fill-outline-color': 'white'
+        }, 'filter': ['>', ['get', 'COUNT'], 0] // filter out points where COUNT = 0
+    }, 'Accidents'); // paints below accidents point layer
+
+    // EXTRA MAP CUSTOMIZATION
+
+    // Add map controls
+    map.addControl(new mapboxgl.FullscreenControl(), 'top-left');
+    map.addControl(new mapboxgl.NavigationControl(), 'top-left');
+    // These are automatically placed on top
+
+    // Change mouse symbolization on hexagons
+    map.on('mouseenter', 'Tessellation', () => {
+        map.getCanvas().style.cursor = 'pointer'; // Switch cursor to pointer when mouse is over POI
+        map.on('mouseleave', 'Tessellation', () => {
+            map.getCanvas().style.cursor = ''; // Switch cursor back when leaving POI
+        });
+    });
+
+    // Layer toggles
+    // Hexgrid toggle
+    document.getElementById('hexlayercheck').addEventListener('change', (e) => {
+        if (e.target.checked) {
+            map.setLayoutProperty('Tessellation', 'visibility', 'visible');
+        } else {
+            map.setLayoutProperty('Tessellation', 'visibility', 'none');
+        }
+    })
+    // Collision point toggle
+    document.getElementById('colllayercheck').addEventListener('change', (e) => {
+        if (e.target.checked) {
+            map.setLayoutProperty('Accidents', 'visibility', 'visible');
+        } else {
+            map.setLayoutProperty('Accidents', 'visibility', 'none');
+        }
+    })
+
+    // Highlight hexagon on mouse hover
+    let hexID = null
+    map.on('mousemove', 'Tessellation', (e) => {
+        // Set hover feature state back to false to remove opacity from previous highlighted polygon
+        map.setFeatureState(
+            { source: 'agg_collision', id: hexID },
+            { hover: false }
+        );
+        hexID = e.features[0].id; // Update hexID to featureID
+        map.setFeatureState(
+            { source: 'agg_collision', id: hexID },
+            { hover: true } // Update hover feature state to TRUE to change opacity of layer to 1
+        );
+    });
+    // If mouse leaves the geojson layer, set all hover states to false and provID variable back to null
+    map.on('mouseleave', 'Tessellation', () => {
+
+        map.setFeatureState(
+            { source: 'agg_collision', id: hexID },
+            { hover: false }
+        );
+        hexID = null;
+    }); 
+
+    //(section kept for future use)------------------------------------------------------------------------------
 
     // STUDY IN UNDERSTANDING 'console.log' AND SEARCHING FOR VALUES
 
@@ -111,15 +179,3 @@ map.on('load', () => {
     // -> y-coordinate: 43.590289 (index 1)
     //------------------------------------------------------------------------------------
 });
-
-// /*--------------------------------------------------------------------
-// Step 5: FINALIZE YOUR WEB MAP
-// --------------------------------------------------------------------*/
-//HINT: Think about the display of your data and usability of your web map.
-//      Update the addlayer paint properties for your hexgrid using:
-//        - an expression
-//        - The COUNT attribute
-//        - The maximum number of collisions found in a hexagon
-//      Add a legend and additional functionality including pop-up windows
-
-
